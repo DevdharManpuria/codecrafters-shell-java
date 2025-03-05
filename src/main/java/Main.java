8df5700529f52d1c6e61935132944be5f24493a1
Devdhar Manpuria <btech10748.23@bitmesra.ac.in>
2025-03-05 16:36:03 +0530

    [any message]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,13 @@
-import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.print("$ ");
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
+      System.out.print("$ ");
+      Scanner scanner = new Scanner(System.in);
+      String input = scanner.nextLine();
+      do {
         System.out.println(input + ": command not found");
+        System.out.print("$ ");
+        input = scanner.nextLine();
+      } while (true);
+      scanner.close();
     }
-}
+  }
