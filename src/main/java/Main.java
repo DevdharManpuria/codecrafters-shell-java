65d190675a4d5b2e0e2587b98d896d655bd773f1
Devdhar Manpuria <btech10748.23@bitmesra.ac.in>
2025-03-05 17:18:06 +0530

    echoip

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,6 +11,11 @@ public class Main {
         if(input.equals("exit 0")){
             break;
         }
+        if (input.startsWith("echo")) {
+            System.out.println(input.substring(5));
+          } else {
+            System.out.println(input + ": command not found");
+          }
       } while (true);
     }
   }
