928e782ca5b9017333fc1e49f06365a4a61b52f1
Devdhar Manpuria <btech10748.23@bitmesra.ac.in>
2025-03-05 22:18:57 +0530

    echo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,21 +1,18 @@
-import java.util.*;
 public class Main {
-    public static void main(String[] args) throws Exception {
+  public static void main(String[] args) throws Exception {
+    // System.out.println("Logs from your program will appear here!");
+    while (true) {
       System.out.print("$ ");
       Scanner scanner = new Scanner(System.in);
       String input = scanner.nextLine();
-      do {
-        System.out.print("$ ");
-        input = scanner.nextLine();
-        if(input.equals("exit 0")){
-            break;
-        }
-        if (input.startsWith("echo")) {
-            System.out.println(input.substring(5));
-          } 
-          else {
-            System.out.println(input + ": command not found");
-          }
-      } while (true);
+      if (input.equals("exit 0")) {
+        break;
+      }
+      if (input.startsWith("echo")) {
+        System.out.println(input.substring(5));
+      } else {
+        System.out.println(input + ": command not found");
+      }
     }
   }
+}
